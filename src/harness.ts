9064092c5717cbc82288ab3fd61db622158392d4
Jim Gillett <503909+jim0thy@users.noreply.github.com>
2025-03-10 10:44:53 +0000

    Ignore tsbuildinfo and comment out harness example code
    Added `tsconfig.tsbuildinfo` to `.gitignore` to prevent build artifacts from being tracked. Commented out example code in `harness.ts` to clean up unused code while preserving it for reference.

--- src/harness.ts
+++ src/harness.ts
@@ -1,33 +1,33 @@
-import { Context } from 'aws-lambda';
-import { handler } from './index.js';
-
-const events = {
-  request_type: 'store_dependency',
-  package_name: "@seccl/foo",
-  repository: "foo",
-  dependencies: {
-    dependencies: {
-      p1: "1.0.0",
-      ["@seccl/p2"]: "0.5.0",
-    }
-  },
-  is_workspace: false
-};
-
-const context: Context = {
-  callbackWaitsForEmptyEventLoop: false,
-  functionName: '',
-  functionVersion: '',
-  invokedFunctionArn: '',
-  memoryLimitInMB: '',
-  awsRequestId: '',
-  logGroupName: '',
-  logStreamName: '',
-  getRemainingTimeInMillis: function(): number { return 0; },
-  done: function(_error?: Error, _result?: any): void { },
-  fail: function(_error: Error | string): void { },
-  succeed: function(_messageOrObject: any): void { }
-};
-
-handler(events, context).then(res => console.log(res));
-
+// import { Context, SQSEvent } from 'aws-lambda'
+// import { handler } from './index.js';
+//
+// const events: SQSEvent[] = {
+//   request_type: 'store_dependency',
+//   package_name: "@seccl/foo",
+//   repository: "foo",
+//   dependencies: {
+//     dependencies: {
+//       p1: "1.0.0",
+//       ["@seccl/p2"]: "0.5.0",
+//     }
+//   },
+//   is_workspace: false
+// };
+//
+// const context: Context = {
+//   callbackWaitsForEmptyEventLoop: false,
+//   functionName: '',
+//   functionVersion: '',
+//   invokedFunctionArn: '',
+//   memoryLimitInMB: '',
+//   awsRequestId: '',
+//   logGroupName: '',
+//   logStreamName: '',
+//   getRemainingTimeInMillis: function(): number { return 0; },
+//   done: function(_error?: Error, _result?: any): void { },
+//   fail: function(_error: Error | string): void { },
+//   succeed: function(_messageOrObject: any): void { }
+// };
+//
+// handler(events, context).then(res => console.log(res));
+//
